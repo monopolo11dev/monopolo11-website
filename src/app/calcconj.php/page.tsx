2749ec92fc13dcf75093f4a50e8939e71c9b8ff0
Bernardo Ruiz <bernardorn21@gmail.com>
2023-10-03 14:48:05 -0600

    feat(patch): remove matomo

--- src/app/calcconj.php/page.tsx
+++ src/app/calcconj.php/page.tsx
@@ -1,11 +1,8 @@
 'use client';
 
-import { push } from '@socialgouv/matomo-next';
 import { redirect } from 'next/navigation';
-import { useEffect } from 'react';
 
 const CalcConjRedirect = () => {
-  useEffect(() => push(['trackEvent', 'calc-conj-redirect']));
   redirect('/calculadora-de-conjuntos');
 };
 
