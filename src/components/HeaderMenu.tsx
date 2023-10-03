66bfa73fafbfdd4688ef1e3d4e7109b7c968c11c
Bernardo Ruiz <bernardorn21@gmail.com>
2023-10-03 13:58:57 -0600

    feat(minor): added menu functionality

--- src/components/HeaderMenu.tsx
+++ src/components/HeaderMenu.tsx
@@ -1,16 +1,37 @@
 'use client';
 
-import { Menu, Group, Center, Container, Title, em } from '@mantine/core';
-import { useMediaQuery } from '@mantine/hooks';
+import {
+  Menu,
+  Group,
+  Center,
+  Container,
+  Title,
+  em,
+  Drawer,
+  ScrollArea,
+  Divider,
+  Box,
+  rem,
+  Burger,
+} from '@mantine/core';
+import { useDisclosure, useMediaQuery } from '@mantine/hooks';
 import { IconChevronDown } from '@tabler/icons-react';
+import { usePathname } from 'next/navigation';
+import { useEffect } from 'react';
 import Link from 'next/link';
 import classes from './HeaderMenu.module.css';
 import { AppRoutes } from '@/constants';
 
 export function HeaderMenu() {
   const isMobile = useMediaQuery(`(max-width: ${em(750)})`);
+  const pathname = usePathname();
+  const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] = useDisclosure(false);
 
   const links = AppRoutes.filter((link) => !link.footerOnly);
+
+  useEffect(() => {
+    closeDrawer();
+  }, [pathname]);
 
   const items = links.map((link) => {
     const menuItems = link.links?.map((item) => (
@@ -41,19 +62,40 @@ export function HeaderMenu() {
   });
 
   return (
-    <header className={classes.header}>
-      <Container size="85%">
-        <div className={classes.inner}>
-          <Link href="/">
-            <Title order={1} size={isMobile ? '24' : '30'}>
-              Monopolo11&apos;s Website
-            </Title>
-          </Link>
-          <Group gap={5} visibleFrom="sm" className="ml-8">
-            {items}
-          </Group>
-        </div>
-      </Container>
-    </header>
+    <Box pb={120}>
+      <header className={classes.header}>
+        <Container size="85%">
+          <div className={classes.inner}>
+            <Link href="/">
+              <Title order={1} size={isMobile ? '24' : '30'}>
+                Monopolo11&apos;s Website
+              </Title>
+            </Link>
+            <Group gap={5} visibleFrom="sm" className="ml-8">
+              {items}
+            </Group>
+            <Burger opened={drawerOpened} onClick={toggleDrawer} hiddenFrom="sm" />
+          </div>
+        </Container>
+      </header>
+      <Drawer
+        opened={drawerOpened}
+        onClose={closeDrawer}
+        size="100%"
+        padding="md"
+        title="Navigation"
+        hiddenFrom="sm"
+        zIndex={1000000}
+      >
+        <ScrollArea h={`calc(100vh - ${rem(80)})`} mx="-md">
+          <Divider my="sm" />
+          {links.map((link) => (
+            <Link href={link.link} className={classes.link}>
+              {link.label}
+            </Link>
+          ))}
+        </ScrollArea>
+      </Drawer>
+    </Box>
   );
 }
