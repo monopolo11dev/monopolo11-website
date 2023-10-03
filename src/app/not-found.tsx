2dd6010d406b90cd324ae8c8b2e371a15962ec8f
Bernardo Ruiz <bernardorn21@gmail.com>
2023-10-03 13:39:44 -0600

    feat(minor): better not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,14 +1,21 @@
 'use client';
 
-import { Button, Title } from '@mantine/core';
+import { Container, Title, Text, Button, Group } from '@mantine/core';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { LEGACY_ROUTES } from '@/constants';
+import { Illustration404 } from '@/components/Illustration404';
+import classes from './not-found.module.css';
 
 const LEGACY_TEXT =
   "Sorry, we couldn't find what you were looking for. You are probably looking for a page that was moved to a legacy page.";
 
-const NOT_FOUND_TEXT = "Sorry, we couldn't find what you were looking for.";
+const NOT_FOUND_TEXT =
+  'Page you are trying to open does not exist. You may have mistyped the address, or thepage has been moved to another URL. If you think this is an error contact support.';
+
+const NOT_FOUND_TITLE = 'Nothing to see here';
+
+const LEGACY_TITLE = 'The page has moved';
 
 function NotFoundPage() {
   const pathname = usePathname();
@@ -16,18 +23,27 @@ function NotFoundPage() {
   const isLegacy = LEGACY_ROUTES.includes(cleanPathname);
 
   return (
-    <div className="w-1/2 flex flex-col items-center m-auto">
-      <Title order={1}>Page not found</Title>
-      <p>
-        {isLegacy ? LEGACY_TEXT : NOT_FOUND_TEXT}
-        <p>{pathname}</p>
-      </p>
-      {isLegacy && (
-        <Button component={Link} href={`https://legacy.monopolo11.com${pathname}`}>
-          Click here to try and redirect
-        </Button>
-      )}
-    </div>
+    <Container className={classes.root}>
+      <div className={classes.inner}>
+        <Illustration404 className={classes.image} />
+        <div className={classes.content}>
+          <Title className={classes.title}>{isLegacy ? LEGACY_TITLE : NOT_FOUND_TITLE}</Title>
+          <Text c="dimmed" size="lg" ta="center" className={classes.description}>
+            {isLegacy ? LEGACY_TEXT : NOT_FOUND_TEXT}
+          </Text>
+          <Group justify="center">
+            <Link href="/">
+              <Button size="md">Take me back to home page</Button>
+            </Link>
+            {isLegacy && (
+              <Link href={`https://legacy.monopolo11.com/${pathname}`}>
+                <Button size="md">Take me to legacy page</Button>
+              </Link>
+            )}
+          </Group>
+        </div>
+      </div>
+    </Container>
   );
 }
 
